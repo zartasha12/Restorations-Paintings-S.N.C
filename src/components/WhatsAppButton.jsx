561e7c9c93d6c9c0d0627a7c456f6c5f3af4bbfb
zartasha12 <zartasha.ehsan12@gmail.com>
2024-03-26 00:03:02 +0100

    updated

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -4,7 +4,7 @@ import { whatsapp } from "../assets/index.js";
 const WhatsAppButton = () => {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone=+3288818283"
+      href="https://wa.me/3288818283"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-8 left-8 w-16 h-16 flex items-center justify-center rounded-full shadow-lg z-10"
